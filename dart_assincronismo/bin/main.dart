42834a1a5f0272241201bc2d3c3cd08de41ce274
juniorlatalisa <paraojuniorler@gmail.com>
2026-06-09 20:52:46 -0300

    Dart: dominando assincronismo e criando comunicação com APIs - Aula 03
    Signed-off-by: juniorlatalisa <paraojuniorler@gmail.com>

--- dart_assincronismo/bin/main.dart
+++ dart_assincronismo/bin/main.dart
@@ -1,16 +1,24 @@
+import 'package:dart_assincronismo/api_key.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 void main() {
   print('Olá mundo');
   // requestData();
-  requestDataAsync();
+  // requestDataAsync();
+  sendDataAsync({
+    "id": "NEW001", //
+    "name": "Flutter", //
+    "lastName": "Dart", //
+    "balance": 5000, //
+  });
 }
 
-void requestData() {
-  String url =
-      'https://gist.githubusercontent.com/juniorlatalisa/257d666dcebae2ff43fba7881628b21c/raw/b816f4c76a2e6c2c2a1afa2e96df212854353396/accounts.json';
+const gist = "https://api.github.com/gists/257d666dcebae2ff43fba7881628b21c";
+const url =
+    'https://gist.githubusercontent.com/juniorlatalisa/257d666dcebae2ff43fba7881628b21c/raw/b816f4c76a2e6c2c2a1afa2e96df212854353396/accounts.json';
 
+void requestData() {
   final futureResponse = http.get(Uri.parse(url));
 
   futureResponse.then((response) {
@@ -25,13 +33,39 @@ void requestData() {
   print('Última coisa a acontecer na função');
 }
 
-void requestDataAsync() async {
-  String url =
-      'https://gist.githubusercontent.com/juniorlatalisa/257d666dcebae2ff43fba7881628b21c/raw/b816f4c76a2e6c2c2a1afa2e96df212854353396/accounts.json';
-
+Future<List<dynamic>> requestDataAsync() async {
   final response = await http.get(Uri.parse(url));
 
-  print(json.decode(response.body)[0]);
+  List<dynamic> lista = json.decode(response.body);
+
+  print(lista[0]);
 
   print('De fato a última coisa a acontecer na função');
+
+  return lista;
+}
+
+void sendDataAsync(Map<String, dynamic> mapAccount) async {
+  List<dynamic> listAccounts = await requestDataAsync();
+  listAccounts.add(mapAccount);
+  String content = json.encode(listAccounts);
+  // print(content);
+
+  final response = await http.post(
+    Uri.parse(gist),
+    headers: {"Authorization": "Bearer $githubApiKey"},
+    body: json.encode({
+      "description": "accounts.json", //
+      "public": true,
+      "files": {
+        "accounts.json": {
+          "content": content, //
+        },
+      },
+    }), //
+  );
+
+  // https://cursos.alura.com.br/classpage/dart-dominando-assincronismo-criando-comunicacao-apis/task/166429
+
+  print(response.statusCode);
 }
